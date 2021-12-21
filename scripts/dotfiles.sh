983d80f95e17b0e3d340ffbf623830537a19747f
Julian Pieles <pielesju@gmail.com>
2021-12-21 23:57:06 +0100

    path change

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd /home/julian/files/
-cp /home/julian/.config/i3/config dot/i3config
-cp /home/julian/.config/i3status/config dot/i3statusconfig
-cp /home/julian/.config/bash/bashrc dot/bashrc
-cp /home/julian/.config/vim/vimrc dot/vimrc
-cp /home/julian/.config/X11/xresources dot/Xresources
-cp /home/julian/.config/X11/xinitrc dot/xinitrc
-cp -r /home/julian/files/scripts dot/
+cp /home/julian/.config/i3/config dotfiles/i3config
+cp /home/julian/.config/i3status/config dotfiles/i3statusconfig
+cp /home/julian/.config/bash/bashrc dotfiles/bashrc
+cp /home/julian/.config/vim/vimrc dotfiles/vimrc
+cp /home/julian/.config/X11/xresources dotfiles/Xresources
+cp /home/julian/.config/X11/xinitrc dotfiles/xinitrc
+cp -r /home/julian/files/scripts dotfiles/
 
