21a83140dd22cb8d6d1d9070c323118bd56a0c74
Julian Pieles <pielesju@gmail.com>
2021-12-28 23:21:46 +0100

    adding xdg user dirs

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -6,5 +6,6 @@ cp /home/julian/.config/bash/bashrc dotfiles/bashrc
 cp /home/julian/.config/vim/vimrc dotfiles/vimrc
 cp /home/julian/.config/X11/xresources dotfiles/Xresources
 cp /home/julian/.config/X11/xinitrc dotfiles/xinitrc
+cp /home/julian/.config/user-dirs.dirs dotfiles/user-dirs.dirs
 cp -r /home/julian/files/scripts dotfiles/
 
